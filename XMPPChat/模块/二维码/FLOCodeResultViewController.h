38616536a6840fc4a84ff3595b9430650eb3dcd4
flolangka <flolangka@gmail.com>
2015-12-18 18:06:24 +0800

    扫一扫

---
+++ XMPPChat/模块/二维码/FLOCodeResultViewController.h
@@ -0,0 +1,15 @@
+//
+//  FLOCodeResultViewController.h
+//  XMPPChat
+//
+//  Created by admin on 15/12/18.
+//  Copyright © 2015年 Flolangka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLOCodeResultViewController : UIViewController
+
+@property (nonatomic, copy) NSString *codeResultStr;
+
+@end
